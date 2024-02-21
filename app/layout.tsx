0dad1736b4b88a2e51fc87dcc9f1a6d4242c0777
kaiowsz <kaionpsa@gmail.com>
2024-02-21 09:59:16 -0300

    feat: route structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+  return (  
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", ibm_plex_sans.variable)}>{children}</body>
     </html>
