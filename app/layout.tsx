b38cf3bdf4033ff807d53da6ff0348948178e87d
kaiowsz <kaionpsa@gmail.com>
2024-02-24 14:57:11 -0300

    feat: Shared components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const ibm_plex_sans = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -20,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (  
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", ibm_plex_sans.variable)}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: "#000000" }
+    }} >
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", ibm_plex_sans.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
