aa4f3374581496fd059b3624de3d7c65ef90b7d4
kaiowsz <kaionpsa@gmail.com>
2024-02-26 07:51:51 -0300

    feat: user actions and middleware

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -44,7 +44,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     }
 }
 
-export async function deleteUser(clerkId: string, user: UpdateUserParams) {
+export async function deleteUser(clerkId: string) {
     try {
         await connectToDatabase();
         
