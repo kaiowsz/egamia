04154fd334e5e49619bff443b765a36c536f5d97
kaiowsz <kaionpsa@gmail.com>
2024-04-11 08:55:59 -0300

    feat: Add wavy background on auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,12 @@
+import { WavyBackground } from '@/components/shared/WavyBackground'
 import React from 'react'
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <main className="auth">{children}</main>
+    <main className="auth">
+      {children}
+      <WavyBackground />  
+    </main>
   )
 }
 
